caec4bdf2a53faac5a1de22d0e3b1dae85d09cdd
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-01-20 14:01:11 +0100

    env test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      pleeee
-    </div>
-  );
+  return <div className="App">dd</div>;
 }
 
 export default App;
