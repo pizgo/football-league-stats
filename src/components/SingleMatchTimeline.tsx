3964612be78cfa99d5dfc63311f067e6f2825074
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-01-25 16:54:30 +0100

    added basic way of display timeline data

--- src/components/SingleMatchTimeline.tsx
+++ src/components/SingleMatchTimeline.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {SingleMatchSchema, SingleMatchTimelineSchema} from "../types/types";
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row, Table} from "react-bootstrap";
 
 interface SingleMatchTimelineProps {
     chosenMatch: SingleMatchSchema;
@@ -11,6 +11,17 @@ const SingleMatchTimeline: React.FC<SingleMatchTimelineProps> = ( {chosenMatch, 
         <Container className="shadow-lg w-75 rounded mt-5" style={{backgroundColor: "#A4BE7B"}}>
             <Row className="justify-content-center fw-bold mt-4">
                 <Col xs lg="4" className="text-center">Timeline</Col>
+            </Row>
+            <Row>
+                <Table responsive className="border-rounded">
+                    {timeline.map((el, key) => (
+                        <tr>
+                            {(el.competitor === "home") ? <td>{el.type}</td> : <td></td>}
+                            <td>{el.matchTime}</td>
+                            {(el.competitor === "away") ? <td>{el.type}</td> : <td></td>}
+                        </tr>
+                    ))}
+                </Table>
             </Row>
         </Container>
     )
