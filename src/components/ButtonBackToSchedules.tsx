6417730bcc4b30ac53a979fd4ec081964f23ce92
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-01-25 20:45:24 +0100

    changing Button color

--- src/components/ButtonBackToSchedules.tsx
+++ src/components/ButtonBackToSchedules.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {Button} from "react-bootstrap";
-import { BiArrowBack } from "react-icons/bi"
-
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { BiArrowBack } from "react-icons/bi";
 
 const ButtonBackToSchedules: React.FC = () => {
-    return (
-            <Link to="/">
-                <Button variant="light" size="lg" className="position-fixed px-5 fw-bold"><BiArrowBack/></Button>
-            </Link>
-    )
-}
+  return (
+    <Link to="/">
+      <Button
+        variant="light"
+        size="lg"
+        className="position-fixed px-5 fw-bold"
+        style={{ backgroundColor: "#D9D9D6" }}
+      >
+        <BiArrowBack />
+      </Button>
+    </Link>
+  );
+};
 
 export default ButtonBackToSchedules;
