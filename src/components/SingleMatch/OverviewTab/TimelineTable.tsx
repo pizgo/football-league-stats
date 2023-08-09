bed3c17a662982c2512dfa253b0a8a1baa938435
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-08-09 14:03:11 +0200

    created foundations for horizontal timeline

--- src/components/SingleMatch/OverviewTab/TimelineTable.tsx
+++ src/components/SingleMatch/OverviewTab/TimelineTable.tsx
@@ -15,23 +15,35 @@ interface TimelineEventProps {
 const TimelineTable: React.FC<TimelineEventProps> = ({timeline}) => {
 
     return (
-        <Table>
-            <tbody>
-            {timeline.map((el, key) => (
-                <TableRow sx={{ "& td": { border: 0 } }}>
-                    <SingleTimelineEvent timelineEvent = {el}
-                                         competitorType="home"
-                                         homeCompetitorStyle="justify-end"
-                                         tooltipPlacement="left" />
-                    <TableCell className="text-center py-1 px-2 bg-neutral-200 w-8 md:w-16 md:text-base">{changeMatchTimeFormat(el.matchTime)}</TableCell>
-                    <SingleTimelineEvent competitorType="away"
-                                         timelineEvent={el}
-                                         homeCompetitorStyle="justify-start"
-                                         tooltipPlacement="right"/>
-                </TableRow>
-            ))}
-            </tbody>
-        </Table>
+        <>
+            <Table className="mb-12">
+                <tbody>
+                {timeline.map((el, key) => (
+                    <TableRow sx={{ "& td": { border: 0 } }}>
+                        <SingleTimelineEvent timelineEvent = {el}
+                                             competitorType="home"
+                                             homeCompetitorStyle="justify-end"
+                                             tooltipPlacement="left" />
+                        <TableCell className="text-center py-1 px-2 bg-neutral-200 w-8 md:w-16 md:text-base">{changeMatchTimeFormat(el.matchTime)}</TableCell>
+                        <SingleTimelineEvent competitorType="away"
+                                             timelineEvent={el}
+                                             homeCompetitorStyle="justify-start"
+                                             tooltipPlacement="right"/>
+                    </TableRow>
+                ))}
+                </tbody>
+            </Table>
+            <div className="overflow-x-scroll">
+                <div className="relative w-96 h-1 bg-primary-100 m-12">
+                    <div className="absolute bottom-1/2 left-4 translate-y-1/2 z-30 bg-white h-5 w-6">
+                        <p className="text-center">20</p>
+                    </div>
+                </div>
+            </div>
+
+        </>
+
+
     )
 }
 export default TimelineTable;
