aa8db4069aa9e65ee09c344badfa525e939291d4
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-08-29 13:51:21 +0200

    function for formatting data on players statistics (changing strings into "pictures")

--- src/components/SingleMatch/LineupsTab/LineupItem.tsx
+++ src/components/SingleMatch/LineupsTab/LineupItem.tsx
@@ -7,24 +7,15 @@ import {
     TableCell, TableContainer, TableHead,
     TableRow
 } from "@mui/material";
+import { PlayerStatistics } from "../../../types/types";
 import {
-    LineupsPlayersStatistics,
-    LineupsSchema,
-    PlayersStatisticsSchema, PlayerStatistics,
-    SingleMatchSchema
-} from "../../../types/types";
-import {changeStatsNameFormat} from "../../../utils/changeStatsNameFormat";
-import {
-    assist, goalsScored,
-    redCard, redCards,
-    scoreChange,
-    scorer,
-    substitutedIn,
-    substitutedOut,
-    substitution,
-    yellowCard
+    redCardsLineup,
+    substitutedInLineup,
+    substitutedOutLineup,
+    yellowCardsLineup
 } from "../../../utils/consts";
 import {BiFootball} from "react-icons/bi";
+import {goalsScoredLineup} from "../../../utils/consts";
 
 interface StatisticsProps {
     playerType: string,
@@ -33,30 +24,23 @@ interface StatisticsProps {
 
 const LineupItem: React.FC<StatisticsProps> = ({ playerType, lineups}) => {
     const formattedStatistics =  (type:string)  => {
-        if (type === goalsScored) {
+        if (type === goalsScoredLineup) {
             return <BiFootball size={20}/>
         }
-        if (type === redCards) {
-            return (
-                <div>
-                    <span className="text-winner text-base md:text-lg">&#8593;</span>
-                    <span className="text-lost text-base md:text-lg">&#8595;</span>
-                </div>
-            )
-        } else if (type === yellowCard) {
-            return <div className="bg-yellow-500 w-4 h-5 rounded"/>
-        } else if (type === redCard) {
+        if (type === redCardsLineup) {
             return <div className="bg-red-700 w-4 h-5 rounded"/>
-        } else if (type === substitutedIn) {
-            return <span className="text-winner">&#8593;</span>
-        } else if (type === substitutedOut) {
-            return <span className="text-lost">&#8595;</span>
-        } else if (type === scorer) {
-            return "scorer: "
-        } else if (type === assist) {
-            return " assist: "
         }
+       if (type === yellowCardsLineup) {
+            return <div className="bg-yellow-500 w-4 h-5 rounded"/>
+        }
+       if (type === substitutedInLineup) {
+            return <span className="text-winner">&#8593;</span>
+        }
+       if (type === substitutedOutLineup) {
+            return <span className="text-lost">&#8595;</span>
+       }
     }
+
     return (
         <div className="col-span-12">
             <div>{playerType}</div>
@@ -65,7 +49,7 @@ const LineupItem: React.FC<StatisticsProps> = ({ playerType, lineups}) => {
                     <p>{player.jersey_number}</p>
                     <p>{player.name}</p>
                     {player.statistics.map((el) => (
-                        <p>{el}</p>))}
+                        <p>{formattedStatistics(el)}</p>))}
                 </div>
             ))}
         </div>
